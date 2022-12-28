5bf7f543ad97a4fd8194dff5ee83dad01515bba1
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2022-12-28 13:21:08 -0500

    Update violin-spectra.py

--- violin-spectra.py
+++ violin-spectra.py
@@ -1,7 +1,13 @@
-write('Hello, *World!* :sunglasses:')
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.write(1234)
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-}))
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
