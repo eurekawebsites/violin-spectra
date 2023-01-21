52712087f01a7cee997b2e8da060952c484b1c6e
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2023-01-21 09:01:53 -0500

    Update violin-spectra.py

--- violin-spectra.py
+++ violin-spectra.py
@@ -4,28 +4,31 @@ from bs4 import BeautifulSoup
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
-from translate import Translator
-import nltk
+import googletrans
 
 st.title("Scientific Paper Summary Generator")
 
-nltk.download('punkt')
-
+# Use the CrossRef API to retrieve the paper's URL based on the DOI
 doi = st.text_input("Enter DOI of the paper:")
+headers = {'accept': 'application/vnd.citationstyles.csl+json'}
+res = requests.get(f'https://api.crossref.org/works/{doi}', headers=headers)
+url = res.json()['message']['URL']
 
-if doi:
-    paper = requests.get(doi, allow_redirects=False).content
-    soup = BeautifulSoup(paper, 'html.parser')
-    text = soup.get_text()
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LexRankSummarizer()
-    summary = summarizer(parser.document, 5)
-    st.write("Summary of the paper:")
-    for sentence in summary:
-        st.write(sentence)
-    st.write("Translations")
-    target_languages = ['fr','es','de']
-    for lang in target_languages:
-        translator= Translator(to_lang=lang)
-        translation = translator.translate(" ".join(str(sentence) for sentence in summary))
-        st.write(f'Summary in {lang} : {translation}')
+# Scrape the paper's text and summarize it
+paper = requests.get(url).content
+soup = BeautifulSoup(paper, 'html.parser')
+text = soup.get_text()
+parser = PlaintextParser.from_string(text, Tokenizer("english"))
+summarizer = LexRankSummarizer()
+summary = summarizer(parser.document, 5)
+st.write("Summary of the paper:")
+for sentence in summary:
+    st.write(sentence)
+
+# Translate the summary into multiple languages using the googletrans library
+st.write("Translations")
+target_languages = ['fr','es','de']
+for lang in target_languages:
+    translator= googletrans.Translator()
+    translation = translator.translate(" ".join(str(sentence) for sentence in summary), dest=lang)
+    st.write(f'Summary in {lang} : {translation.text}')
