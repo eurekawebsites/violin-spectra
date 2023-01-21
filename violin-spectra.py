7f09d18e87ed2026c149c35130c74df7b12c79f6
eurekawebsites <121565788+eurekawebsites@users.noreply.github.com>
2023-01-21 09:18:42 -0500

    Update violin-spectra.py

--- violin-spectra.py
+++ violin-spectra.py
@@ -4,31 +4,31 @@ from bs4 import BeautifulSoup
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
-import googletrans
 
 st.title("Scientific Paper Summary Generator")
 
-# Use the CrossRef API to retrieve the paper's URL based on the DOI
 doi = st.text_input("Enter DOI of the paper:")
-headers = {'accept': 'application/vnd.citationstyles.csl+json'}
-res = requests.get(f'https://api.crossref.org/works/{doi}', headers=headers)
-url = res.json()['message']['URL']
 
-# Scrape the paper's text and summarize it
-paper = requests.get(url).content
-soup = BeautifulSoup(paper, 'html.parser')
-text = soup.get_text()
-parser = PlaintextParser.from_string(text, Tokenizer("english"))
-summarizer = LexRankSummarizer()
-summary = summarizer(parser.document, 5)
-st.write("Summary of the paper:")
-for sentence in summary:
-    st.write(sentence)
-
-# Translate the summary into multiple languages using the googletrans library
-st.write("Translations")
-target_languages = ['fr','es','de']
-for lang in target_languages:
-    translator= googletrans.Translator()
-    translation = translator.translate(" ".join(str(sentence) for sentence in summary), dest=lang)
-    st.write(f'Summary in {lang} : {translation.text}')
+if doi:
+    headers = {'accept': 'application/vnd.citationstyles.csl+json'}
+    res = requests.get(f'https://api.crossref.org/works/{doi}', headers=headers)
+    if res.status_code != 200:
+        st.write("An error occured: ", res.text)
+    else:
+        try:
+            url = res.json()['message']['URL']
+        except ValueError as e:
+            st.write("An error occured: Invalid response, response is not in json format")
+            return
+    paper = requests.get(url)
+    if paper.status_code != 200:
+        st.write("An error occured: ", paper.text)
+    else:
+        soup = BeautifulSoup(paper.content, 'html.parser')
+        text = soup.get_text()
+        parser = PlaintextParser.from_string(text, Tokenizer("english"))
+        summarizer = LexRankSummarizer()
+        summary = summarizer(parser.document, 5)
+        st.write("Summary of the paper:")
+        for sentence in summary:
+            st.write(sentence)
